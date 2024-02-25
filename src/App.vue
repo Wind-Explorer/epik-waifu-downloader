a0ae93bd72bd35d825b4a2c91b10c7af97faae4f
Wind-Explorer <windexplorer@outlook.com>
2024-02-25 12:00:24 +0800

    Minimalized functionalities & added transitions

--- src/App.vue
+++ src/App.vue
@@ -2,66 +2,37 @@
   <div class="root">
     <div class="relative">
       <div class="flex flex-row">
-        <div class="flex flex-col gap-4 p-4 border-r-2 dark:border-neutral-700 bg-neutral-100 dark:bg-neutral-800 z-50">
-          <fwb-button color="default" square shadow="blue" @click="fetchImage()">
-            <div class="flex flex-col gap-2 text-left">
-              <svg-icon type="mdi" :path="mdiRefresh" />
-              <span>New Waifu</span>
-            </div>
-          </fwb-button>
+        <div
+          class="flex flex-col justify-between p-4 border-r-2 dark:border-neutral-700 bg-neutral-100 dark:bg-neutral-800 z-50">
+          <div class="flex flex-col gap-4">
+            <fwb-button color="default" square shadow="blue" @click="fetchImage()">
+              <div class="flex flex-col gap-2 text-left">
+                <svg-icon type="mdi" :path="mdiRefresh" />
+                <span>New Waifu</span>
+              </div>
+            </fwb-button>
 
-          <fwb-button color="alternative" square @click="openUrl(imageSrc)">
-            <div class="flex flex-col gap-2 text-left">
-              <svg-icon type="mdi" :path="mdiImageSearchOutline"></svg-icon>
-              <span>See Source</span>
-            </div>
-          </fwb-button>
-
-          <fwb-button color="alternative" square @click="isShowModal = true">
-            <div class="flex flex-col gap-2 text-left">
-              <svg-icon type="mdi" :path="mdiAccountDetails"></svg-icon>
-              <span>
-                by <p class="text-blue-500 font-bold">{{ artistName }}</p>
-              </span>
-            </div>
-          </fwb-button>
+            <fwb-button color="alternative" square @click="openUrl(imageSrc)">
+              <div class="flex flex-col gap-2 text-left">
+                <svg-icon type="mdi" :path="mdiImageSearchOutline"></svg-icon>
+                <span>See Source</span>
+              </div>
+            </fwb-button>
+          </div>
+          <!-- artist name   
+          <div class="flex flex-col gap-2 text-left dark:text-white bg-neutral-700 p-3 rounded-lg">
+            <p class="text-blue-500 font-bold">{{ artistName }}</p>
+          </div> -->
         </div>
         <div id="img-div" class="relative w-full h-screen">
-          <img class="absolute inset-0 blur-3xl scale-110 m-auto" :src="apiResponseUrl" alt="waifu image" />
-          <img class="absolute inset-0 max-h-screen m-auto" :src="apiResponseUrl" alt="waifu image" />
+          <img id="wfimgbg" class="absolute inset-0 blur-3xl scale-110 m-auto transition-all duration-1000"
+            :src="apiResponseUrl" alt="waifu image" />
+          <img id="wfimg" class="absolute inset-0 max-h-screen m-auto transition-all duration-1000" :src="apiResponseUrl"
+            alt="waifu image" />
         </div>
       </div>
     </div>
   </div>
-
-
-  <fwb-modal v-if="isShowModal" @close="closeModal">
-    <template #body>
-      <div class="flex flex-row justify-between">
-        <div class="flex items-center text-lg">
-          <span class="dark:text-white">
-            Find
-            <p class="text-blue-500">{{ artistName }}</p>
-            on the internet
-          </span>
-        </div>
-        <div class="flex flex-row gap-4 h-min my-auto">
-          <FwbButton color="default">
-            <p>Deviant Art</p>
-          </FwbButton>
-          <FwbButton color="default">
-            <p>Pateron</p>
-          </FwbButton>
-          <FwbButton color="default">
-            <p>Pixiv</p>
-          </FwbButton>
-          <FwbButton color="default">
-            <p>Twitter</p>
-          </FwbButton>
-        </div>
-      </div>
-    </template>
-  </fwb-modal>
 </template>
 
 <script setup>
@@ -71,6 +42,12 @@ import SvgIcon from '@jamescoyle/vue-icon';
 import { mdiRefresh, mdiImageSearchOutline, mdiAccountDetails } from '@mdi/js';
 import { FwbButton, FwbModal } from 'flowbite-vue'
 
+let apiResponseUrl = ref("");
+let imageSrc = ref("");
+let artistName = ref("");
+
+let img = null;
+let imgbg = null;
 
 const apiUrl = 'https://api.waifu.im/search';
 const params = {
@@ -79,16 +56,26 @@ const params = {
   width: '>=1920'
 };
 
-let apiResponseUrl = ref("");
-let imageSrc = ref("");
-let artistName = ref("");
-
-let isShowModal = ref(false);
-
 const queryParams = new URLSearchParams(params);
 const requestUrl = `${apiUrl}?${queryParams}`;
 
+function startChangeImg() {
+  img.classList.add('blur-3xl', 'opacity-0');
+  setTimeout(() => {
+    imgbg.classList.add('saturate-0', 'opacity-0');
+  }, 500);
+}
+
+function stopChangeImg() {
+
+  setTimeout(() => {
+    imgbg.classList.remove('saturate-0', 'opacity-0');
+    img.classList.remove('blur-3xl', 'opacity-0');
+  }, 1500);
+}
+
 function fetchImage() {
+  startChangeImg();
   fetch(requestUrl)
     .then(response => {
       if (response.ok) {
@@ -104,7 +91,12 @@ function fetchImage() {
     })
     .catch(error => {
       console.error('An error occurred:', error.message);
-    });
+    })
+    .finally(() => {
+      setTimeout(() => {
+        stopChangeImg();
+      }, 500);
+    })
 }
 
 async function openUrl(url) {
@@ -120,6 +112,8 @@ function closeModal() {
 }
 
 onMounted(async () => {
+  img = document.getElementById('wfimg');
+  imgbg = document.getElementById('wfimgbg');
   fetchImage();
 });
 
